1bf46939667fa3c88d6b66facfcfe15a189eb771
Niclas <niclas@niclas.xyz>
2024-12-24 18:57:39 +0100

    Added dark light toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
-import LogViewer from "@/LogViewer.jsx";
+import LogViewer from "@/LogViewer.jsx"
+import {ThemeProvider} from "@/components/theme-provider"
+import ThemeToggle from "@/components/theme-toggle.jsx";
 
 function App() {
-
-  return (
-    <>
-     <LogViewer />
-    </>
-  )
+    return (
+        <ThemeProvider defaultTheme="system" storageKey="app-theme">
+            <div className="flex flex-col min-h-screen">
+                <div className="flex justify-end p-4">
+                    <ThemeToggle/>
+                </div>
+                <LogViewer/>
+            </div>
+        </ThemeProvider>
+    )
 }
 
 export default App
